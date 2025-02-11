13253ce776501fef2172ebe184a1eab82a182212
okvla <hepigo12345@gmail.com>
2025-02-11 16:22:54 +0200

    Upload NewsApp

---
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.loc.newsapp.util
+
+object Constants {
+
+    const val USER_SETTINGS = "user_settings"
+    const val APP_ENTRY = "app_entry"
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val API_KEY = "88f776d872074a29bd411a3ba185c484"
+
+}
